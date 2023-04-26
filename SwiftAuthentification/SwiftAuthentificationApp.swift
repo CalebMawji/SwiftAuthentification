11d3ff5af1dc5cd5e48dedd0ec5b2ab9f2f5cf86
CalebMawji <cmawji@tfs.ca>
2023-04-26 14:57:46 -0400

    Initial Commit

---
+++ SwiftAuthentification/SwiftAuthentificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAuthentificationApp.swift
+//  SwiftAuthentification
+//
+//  Created by Caleb Mawji on 2023-04-26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAuthentificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
